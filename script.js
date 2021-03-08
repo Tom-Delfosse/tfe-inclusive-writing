3c6813c7a5fc9a220ad53eb492553a7223402369
Tom_Delfosse <Tom-Delfosse@users.noreply.github.com>
2021-03-08 15:34:13 +0100

    Added a cancel and a copy to clipboard button + some other small changes.

--- script.js
+++ script.js
@@ -1,60 +1,91 @@
 "use strict";
-let contentEdit = document.querySelector(".div");
-let ContentBtn = document.querySelector(".div-btn");
+let btnCheck = document.querySelector(".btn--check");
+let btnCancel = document.querySelector(".btn--cancel");
+let btnCopy = document.querySelector(".btn--copy");
 let textArea = document.querySelector(".textarea");
 let userText = [];
 let newText = [];
+let text = '';
 
 // window.addEventListener("load", (e) =>{
 //     return textCheck();
 // })
-ContentBtn.addEventListener("click", (e) =>{
-    e.preventDefault();
-    return textCheck();
+
+btnCheck.addEventListener("click", (e) =>{
+    // e.preventDefault();
+    return textCompact();
 })
 
-function textCheck() {
+btnCancel.addEventListener('click', (e) =>{
+    // ce bouton permet simplement d'annuler les modifications effectuées.
+    // e.preventDefault();
+    console.clear();
+    newText = text;
+    btnCancel.setAttribute('disabled', true);
+    textJoin();
+})
+
+btnCopy.addEventListener('click', (e) =>{
+    // Ce bouton permet tout simplement de copier dans le presse-papier le texte modifié.
+    navigator.clipboard.writeText(newText).then(function() {
+        console.log('Le texte fut copié dans le presse papier avec succès !')
+    }, function() {
+        console.log('Une erreur est survenue, impossible de copier dans le presse papier.')
+    })
+})
+
+function textCompact() {
     console.clear();
     userText = [];
-    let text = textArea.value;
+    text = textArea.value;
 
-    // Tout d'abord, on vient découper le texte en paragraphes, que l'on vient stocker dans un tableau.
+    // On check si le textArea est vide et si c'est le cas, on annule la modification du site.
+    if (!text) {
+        return alert("ajoutez du texte pour qu'il soit converti !")
+    }
+
+    // Si le textArea contient du texte, on vient découper le texte en paragraphes, que l'on vient stocker dans un tableau.
     // Pour cela, on utilise une expression régulière qui vient cibler uniquement les retours à la ligne.
     userText = text.replace(/\n?\n/g, '$1|').split('$1|');
+
     // Ensuite, on vient découper et remplacer le tableau en tableau comportant les phrases, classées par paragraphes.
     userText.forEach((e, index) => {
         userText[index] = e.replace(/([.?!])\s*(?=[A-Z])/g, '$1|').split('|');
         userText[index].forEach((subElement, subIndex) => {
 
             // c'est ici que l'on check pour les différents mots à changer.
-            if (subElement.match(/(essai)/g)){
-                userText[index][subIndex] = subElement.replace(/(essai)/g, 'chaton')
-            }            
+            if (subElement.match(/(Maintenant)/g)){
+                userText[index][subIndex] = subElement.replace(/(Maintenant)/g, 'chaton')
+            }
         });
 
         // Après avoir corrigé le texte, on vient rassembler les paragraphes afin de supprimer les caractères inutiles qui permettent de structurer les tableaux.
         userText[index] = userText[index].join(' ')
     });
-    
+    //Ici, on désactive l'état "disabled" du bouton permettant d'annuler les changements.
+    btnCancel.removeAttribute('disabled');
+    btnCopy.removeAttribute('disabled');
+    // Ici, on applique le texte dans une nouvelle variable en ajouter deux retours à la ligne entre chaque paragraphe.
+    newText = userText.join('\n\n')
 
     textJoin();
 }
 
-
-
-
-
-
 function textJoin() {
-// Cette fonction permet ensuite de rassembler le texte dans une nouvelle variable avant de l'attribuer au textarea
-    newText = userText.join('\n\n')
-    textArea.classList.add("textarea--checked")
+    // Cette fonction permet simplement de renvoyer le texte dans la zone éditable directement dans le DOM.
+
+    textArea.classList.add("textarea--checked");
     textArea.value = newText;
 }
 
 
 
+
+
+
 // ____TO_DO____
 // Ajouter une classe succès ! ✅
-// Ajouter un bouton de retour en arrière
-// Ajouter un bouton 'copier dans le presse-papier'
+// Ajouter un bouton de retour en arrière ✅
+// Ajouter un bouton 'copier dans le presse-papier' ✅
+// Si pas de texte, ajouter une option "ajoutez du texte pour le convertir !" ✅
+// Si rien de changé, ajouter une nouvelle classe 'rien ne nécessite d'êtrem modifié !'
