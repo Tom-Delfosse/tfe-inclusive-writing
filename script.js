ec3efc40f5514457ce09b909390bbfc7bee0fab4
Tom_Delfosse <Tom-Delfosse@users.noreply.github.com>
2021-03-08 12:33:01 +0100

    Fixed overwriting issues + added function to join text once it's been modified

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 let contentEdit = document.querySelector(".div");
 let ContentBtn = document.querySelector(".div-btn");
 let textArea = document.querySelector(".textarea");
-let UserText = [];
-
+let userText = [];
+let newText = [];
 window.addEventListener("load", (e) =>{
-    return TextCheck();
+    return textCheck();
 
 })
 // ContentBtn.addEventListener("click", (e) =>{
@@ -13,33 +13,46 @@ window.addEventListener("load", (e) =>{
     // return TextCheck();
 // })
 
-function TextCheck() {
-    console.clear();
-    UserText = [];
+function textCheck() {
+    // console.clear();
+    userText = [];
     let text = textArea.value;
 
     // Tout d'abord, on vient découper le texte en paragraphes, que l'on vient stocker dans un tableau.
     // Pour cela, on utilise une expression régulière qui vient cibler uniquement les retours à la ligne.
-    UserText = text.replace(/\n?\n/g, '$1|').split('$1|');
+    userText = text.replace(/\n?\n/g, '$1|').split('$1|');
    
     // Ensuite, on vient découper et remplacer le tableau en tableau comportant les phrases, classées par paragraphes.
-    UserText.forEach((e, index) => {
-        UserText[index] = e.replace(/([.?!])\s*(?=[A-Z])/g, '$1|').split('|');
-        UserText[index].forEach(subElement => {
-            console.log(subElement)
-            // if (subElement.match(/toutefois/g)){
-            subElement.replace(/\w/g, 'noix')
-                // console.log("je ici ! " + subElement)
-            // }
+    userText.forEach((e, index) => {
+        userText[index] = e.replace(/([.?!])\s*(?=[A-Z])/g, '$1|').split('|');
+        userText[index].forEach((subElement, subIndex) => {
+
+            // c'est ici que l'on check pour les différents mots à changer.
+            if (subElement.match(/(essai)/g)){
+                userText[index][subIndex] = subElement.replace(/(essai)/g, 'piñata')
+            }            
         });
     });
 
 
-    // console.log(UserText);
-     // let p = document.createElement("p");
-    // p.innerHTML = phrases;
-    // document.body.appendChild(p);
-    // let newText = text.replace(/femme/g,  'personne');
-    // contentEdit.innerHTML = newText ; 
 
+
+    textJoin();
+}
+
+
+
+
+
+
+function textJoin() {
+// Cette fonction permet ensuite de rassembler le texte dans une nouvelle variable avant de l'attribuer au textarea
+
+
+
+
+
+    newText = userText;
+    textArea.value = newText;
+// ajouter une classe "succès!"
 }
