73da9b6132edeceb1cf8f0a325702c37c9c80dc8
Tom_Delfosse <Tom-Delfosse@users.noreply.github.com>
2021-03-08 14:44:40 +0100

    Stable build — 0.0.2
    Added a custom class telling the user the transformation was successful + some other fixes

--- script.js
+++ script.js
@@ -4,24 +4,23 @@ let ContentBtn = document.querySelector(".div-btn");
 let textArea = document.querySelector(".textarea");
 let userText = [];
 let newText = [];
-window.addEventListener("load", (e) =>{
-    return textCheck();
 
-})
-// ContentBtn.addEventListener("click", (e) =>{
-//     e.preventDefault();
-    // return TextCheck();
+// window.addEventListener("load", (e) =>{
+//     return textCheck();
 // })
+ContentBtn.addEventListener("click", (e) =>{
+    e.preventDefault();
+    return textCheck();
+})
 
 function textCheck() {
-    // console.clear();
+    console.clear();
     userText = [];
     let text = textArea.value;
 
     // Tout d'abord, on vient découper le texte en paragraphes, que l'on vient stocker dans un tableau.
     // Pour cela, on utilise une expression régulière qui vient cibler uniquement les retours à la ligne.
     userText = text.replace(/\n?\n/g, '$1|').split('$1|');
-   
     // Ensuite, on vient découper et remplacer le tableau en tableau comportant les phrases, classées par paragraphes.
     userText.forEach((e, index) => {
         userText[index] = e.replace(/([.?!])\s*(?=[A-Z])/g, '$1|').split('|');
@@ -29,13 +28,14 @@ function textCheck() {
 
             // c'est ici que l'on check pour les différents mots à changer.
             if (subElement.match(/(essai)/g)){
-                userText[index][subIndex] = subElement.replace(/(essai)/g, 'piñata')
+                userText[index][subIndex] = subElement.replace(/(essai)/g, 'chaton')
             }            
         });
+
+        // Après avoir corrigé le texte, on vient rassembler les paragraphes afin de supprimer les caractères inutiles qui permettent de structurer les tableaux.
+        userText[index] = userText[index].join(' ')
     });
-
-
-
+    
 
     textJoin();
 }
@@ -47,12 +47,14 @@ function textCheck() {
 
 function textJoin() {
 // Cette fonction permet ensuite de rassembler le texte dans une nouvelle variable avant de l'attribuer au textarea
-
-
-
-
-
-    newText = userText;
+    newText = userText.join('\n\n')
+    textArea.classList.add("textarea--checked")
     textArea.value = newText;
-// ajouter une classe "succès!"
 }
+
+
+
+// ____TO_DO____
+// Ajouter une classe succès ! ✅
+// Ajouter un bouton de retour en arrière
+// Ajouter un bouton 'copier dans le presse-papier'
