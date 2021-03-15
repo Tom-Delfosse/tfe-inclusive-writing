517548395e779f55d94645e0534a4750d7834ece
Tom_Delfosse <Tom-Delfosse@users.noreply.github.com>
2021-03-15 16:04:35 +0100

    0.0.3 — the change to array-based checking
    Added new data to specify the gender of the original word;
    completely changed the output of the text to have it set within <p> tags inside a <div>;
    added custom <span> tags correctly highlighting the modified words;
    completely fixed the copy/paste feature;
    added an alert if the original text didn't need any fix;
    changed the core algorithm to support array-based words;
    added button for each span (unusable in this update, will come later)
    removed Herobrine.

--- script.js
+++ script.js
@@ -3,18 +3,32 @@ const btnCheck = document.querySelector(".btn--check");
 const btnCancel = document.querySelector(".btn--cancel");
 const btnCopy = document.querySelector(".btn--copy");
 const textArea = document.querySelector(".textarea");
-const data = import('./checker.json');
 let userText = [];
 let newText = [];
 let text = '';
+let words = '';
 
+
+function checkerLoader(){
+    // Cette fonction a pour simple objectif de récupérer les données JSON de la base de donnée et de les attribuer dans une variable accessible sans faire de requêtes supplémentaires.
+    fetch('./checker.json')
+    .then(function (response) { return response.json()})
+    .then(function (data) {
+        return words = data;
+    }).catch(function (error) {
+        console.error("Il y'a eu un problème avec l'obtention des données.")
+        console.error(error)
+    })
+}
+checkerLoader();
+
+window.addEventListener("load", (e) =>{
+    // Cet écouteur d'évenements a pour objectif d'attendre que les données soient bien téléchargées afin de donner accès aux modifications.
+    btnCheck.removeAttribute('disabled');
+})
 
 btnCheck.addEventListener("click", (e) =>{
     return textCompact();
-})
-
-window.addEventListener("load", (e) =>{
-    console.log(data)
 })
 
 btnCancel.addEventListener('click', (e) =>{
@@ -22,20 +36,13 @@ btnCancel.addEventListener('click', (e) =>{
     console.clear();
     newText = text;
     btnCancel.setAttribute('disabled', true);
-    textJoin();
+    textbackinTxtArea();
 })
 
-btnCopy.addEventListener('click', (e) =>{
-    // Ce bouton permet tout simplement de copier dans le presse-papier le texte modifié.
-    navigator.clipboard.writeText(newText).then(function() {
-        console.log('Le texte fut copié dans le presse papier avec succès !')
-    }, function() {
-        console.log('Une erreur est survenue, impossible de copier dans le presse papier.')
-    })
-})
+
 
 function textCompact() {
-    console.clear();
+    // console.clear();
     userText = [];
     text = textArea.value;
 
@@ -53,52 +60,113 @@ function textCompact() {
         userText[index] = e.replace(/([.?!])\s*(?=[A-Z])/g, '$1|').split('|');
         userText[index].forEach((subElement, subIndex) => {
 
-            // c'est ici que l'on check pour les différents mots à changer.
-            if (subElement.match(/(Normalement)/g)){
-                userText[index][subIndex] = subElement.replace(/(Normalement)/g, 'Étonnamment')
-            }
 
-            // switch (true) {
-            //     case subElement.test(maintenant):
-            //         console.log("Maintenant est ici !")
-            //     break;
-            //     default: 
-            //     console.log('aucun changement à effectuer !');
-            // }
+            words.forEach((word, index) => {
+                let regex = new RegExp('\\b'+'(' + word.toCheck + ')'+'\\b', 'g')
+
+                if (subElement.match(regex)) {                
+                    subElement = subElement.replace(regex, '<span class="corrected">'+word.checked+'</span>');
+                    // subElement = subElement.replace(regex, word.checked);
+                }
+            })
+            userText[index][subIndex] = subElement
         });
 
         // Après avoir corrigé le texte, on vient rassembler les paragraphes afin de supprimer les caractères inutiles qui permettent de structurer les tableaux.
         userText[index] = userText[index].join(' ')
     });
+
     //Ici, on désactive l'état "disabled" du bouton permettant d'annuler les changements.
     btnCancel.removeAttribute('disabled');
     btnCopy.removeAttribute('disabled');
     // Ici, on applique le texte dans une nouvelle variable en ajouter deux retours à la ligne entre chaque paragraphe.
-    newText = userText.join('\n\n')
+    // newText = userText.join('\n\n')
+    newText = userText
 
-    textJoin();
+    if (newText === text ){
+        alert('aucune modification effectuée !')
+    }
+
+    textInNewDiv();
 }
 
-function textJoin() {
-    // Cette fonction permet simplement de renvoyer le texte dans la zone éditable directement dans le DOM.
+function textInNewDiv(){
+    let textDiv = document.createElement("div")
+    userText.forEach(paragraph => {
+        // console.log(paragraph)
+        let p = document.createElement("p");
+        p.innerHTML = paragraph;
+        textDiv.appendChild(p)
+    })
+    textDiv.classList.add("text-container")
+    document.body.replaceChild(textDiv, textArea)
+    btnCheck.setAttribute('disabled', true);
+    spanDetector();
 
-    textArea.classList.add("textarea--checked");
-    textArea.value = newText;
+}
+
+function spanDetector() {
+    let spanList = document.querySelectorAll('.corrected')
+    spanList.forEach(span => {
+        let spanBtn = document.createElement("button");
+        spanBtn.classList.add("btn", "btn--delete");
+        spanBtn.innerHTML = "supprimer"
+        span.appendChild(spanBtn);
+    })   
+    
+    let spanBtnList = document.querySelectorAll('.btn--delete');
+    spanBtnList.forEach((button, index) => {
+        button.addEventListener("click", (e) =>{
+            // console.log(spanList[index].innerHTML)
+            
+            words.forEach(word => {
+                if (spanList[index].innerHTML.match(word.checked)) {
+                    console.log("même mot")
+                    console.log(word.checked)
+                }
+            })
+        })
+    })
+}
+
+function textbackinTxtArea(){
+    let textDiv = document.querySelector(".text-container")
+    textArea.innerHTML = newText;
+    document.body.replaceChild(textArea, textDiv)
+    btnCheck.removeAttribute('disabled');
 }
 
 
+btnCopy.addEventListener('click', (e) =>{
+    // Ce bouton permet tout simplement de copier dans le presse-papier le texte modifié.
+    copyToClip();
+})
+
+function copyToClip() {
+    let textCopied = document.querySelector(".text-container").innerText.replace(/\B(supprimer)/g, '');
+    console.log(textCopied)
+
+
+    // console.log(textCopied)
+    navigator.clipboard.writeText(textCopied).then(function() {
+        console.log('Le texte fut copié dans le presse papier avec succès !')
+    }, function() {
+        console.log('Une erreur est survenue, impossible de copier dans le presse papier.')
+    })
+}
 // ____TO_DO____
 // Ajouter une classe succès ! ✅
 // Ajouter un bouton de retour en arrière ✅
-// Ajouter un bouton 'copier dans le presse-papier' ✅
+// Ajouter un bouton 'copier dans le presse-papier' ❌ Nécessite modification : supprimer les spans. UPDATE : ✅
 // Si pas de texte, ajouter une option "ajoutez du texte pour le convertir !" ✅
-// Si rien de changé, ajouter une nouvelle classe 'rien ne nécessite d'êtrem modifié !'
-// Highlight les endroits changés dans un span et possibilité de les annuler.
-
+// Si rien de changé, ajouter une nouvelle classe 'rien ne nécessite d'êtrem modifié !' ✅
+// Highlight les endroits changés dans un span✅
+// possibilité d'annuler les changements localement.
+// afficher l'explication au hover du span.
+// montrer que l'article a déjà été déjà été modifié
 
 // __LATER__
 // Faire les options de modifications du site.
 // Option pour personnaliser les modifications à effectuer
 // Stocker ces données perso dans le LocalStorage
-// Highlighter les changements effectués ?
 // Montrer, pendant la modification du site, que le site est en train de travailler dans le background.
