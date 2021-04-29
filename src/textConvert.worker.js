1d529a13d02dcc145d7b07f36bc52b46b28c5979
Tom_Delfosse <Tom-Delfosse@users.noreply.github.com>
2021-04-29 11:54:53 +0200

    complete fix

--- src/textConvert.worker.js
+++ src/textConvert.worker.js
@@ -13,18 +13,19 @@ export const textConverter = async (textToConvert, array) => {
     textToConvert.forEach((el, index) => {
       el.forEach((subEl, subIndex) => {
         for (let i = 0; i < array.length; i++) {
-          const regexToCheck = new RegExp('(\\b)(' + array[i].toCheck + ')(?!‧|[A-zÀ-ú])', 'gi')
+          const regexToCheck = new RegExp('(?:\\s)(' + array[i].toCheck + ')(?!‧|[A-zÀ-ú])', 'gi')
 
           if (subEl.match(regexToCheck)) {
             console.log(array[i].toCheck)
-            // subEl = subEl.replace(regexToCheck, ' ' + array[i].checked)
-            // const firstLetter = subEl.charAt(0).toUpperCase()
-            // console.log(firstLetter)
-            // subEl = firstLetter + subEl.substring(1)
+            subEl = subEl.replace(regexToCheck, ' ' + array[i].checked)
+            const firstLetter = subEl.charAt(0).toUpperCase()
+            console.log(firstLetter)
+            subEl = firstLetter + subEl.substring(1)
 
-            // const regexAddSpan = new RegExp('\\b(' + array[i].checked + ')(?![A-zÀ-ú])(?!‧)', 'gi')
-            subEl = subEl.replace(regexToCheck, '<span contenteditable="false" class="corrected corrected--' + array[i].wordID + '">' + array[i].checked + '<button ref="btnDelete" class="btn btn--delete">X</button></span>')
-            // console.log(subEl)
+            const regexAddSpan = new RegExp('(?:\\s)(' + array[i].checked + ')(?!<button)', 'gi')
+            // console.log(regexAddSpan)
+            subEl = subEl.replace(regexAddSpan, ' <span contenteditable="false" class="corrected corrected--' + array[i].wordID + '">$1<button ref="btnDelete" class="btn btn--delete">X</button></span>')
+            console.log(subEl)
             continue
           } else {
             const firstLetter = subEl.charAt(0).toUpperCase()
@@ -32,7 +33,6 @@ export const textConverter = async (textToConvert, array) => {
             continue
           }
         }
-        // subEl = subEl.substring(1)
         textToConvert[index][subIndex] = subEl
       })
       textToConvert[index] = textToConvert[index].join(' ')
